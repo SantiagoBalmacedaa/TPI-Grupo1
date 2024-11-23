b960842069ab11070ceec6062ebf17da2c0e1a3e
AgustinMeza5 <agustinmmeza05@gmail.com>
2024-11-23 14:25:05 -0300

    Modificación visual: comida y segmentos

--- tpi_juego_3.py
+++ tpi_juego_3.py
@@ -9,24 +9,28 @@ high_score = 0
 
 wn = turtle.Screen()
 wn.title("Juego de la Serpiente")
-wn.bgcolor ("black")
-wn.setup (width = 600,height = 600)
-wn.tracer (0)
+wn.bgcolor("black")
+wn.setup(width=600, height=600)
+wn.tracer(0)
 
 cabeza = turtle.Turtle()
-cabeza.speed (0)
+cabeza.speed(0)
 cabeza.shape("square")
 cabeza.color("white")
 cabeza.penup()
-cabeza.goto(0,0)
-cabeza.direction= "stop"
+cabeza.goto(0, 0)
+cabeza.direction = "stop"
+
+# Colores y formas
+colores = ["red", "blue", "green", "yellow", "purple", "orange", "pink", "cyan"]
+formas = ["circle", "square", "triangle"]
 
 comida = turtle.Turtle()
-comida.speed (0)
+comida.speed(0)
 comida.shape("circle")
 comida.color("red")
 comida.penup()
-comida.goto(0,100)
+comida.goto(0, 100)
 
 segmentos = []
 
@@ -35,9 +39,8 @@ texto.speed(0)
 texto.color("white")
 texto.penup()
 texto.hideturtle()
-texto.goto(0,260)
-texto.write("Score: 0       High Score: 0", align = "center", font =("Courier", 24, "normal"))
-
+texto.goto(0, 260)
+texto.write("Score: 0       High Score: 0", align="center", font=("Courier", 24, "normal"))
 
 def arriba():
     cabeza.direction = "up"
@@ -76,27 +79,35 @@ while True:
 
     if cabeza.xcor() > 280 or cabeza.xcor() < -280 or cabeza.ycor() > 280 or cabeza.ycor() < -280:
         time.sleep(1)
-        cabeza.goto(0,0)
+        cabeza.goto(0, 0)
         cabeza.direction = "stop"
 
         for segmento in segmentos:
-            segmento.goto(2000,2000)
+            segmento.goto(2000, 2000)
 
         segmentos.clear()
 
         score = 0
         texto.clear()
-        texto.write("Score: {}      High Score: {}".format(score, high_score), align = "center", font =("Courier", 24, "normal"))
+        texto.write("Score: {}      High Score: {}".format(score, high_score), align="center", font=("Courier", 24, "normal"))
 
     if cabeza.distance(comida) < 20:
-        x = random.randint(-250,250)
-        y = random.randint(-250,250)
-        comida.goto(x,y)
+        # Guardar el color y formas
+        color_comida = comida.color()[0]
+        forma_comida = comida.shape()
+
+        x = random.randint(-250, 250)
+        y = random.randint(-250, 250)
+        comida.goto(x, y)
+
+        # Cambiar a un nuevo color y forma
+        comida.color(random.choice(colores))
+        comida.shape(random.choice(formas))
 
         nuevo_segmento = turtle.Turtle()
-        nuevo_segmento.speed (0)
-        nuevo_segmento.shape("square")
-        nuevo_segmento.color("grey")
+        nuevo_segmento.speed(0)
+        nuevo_segmento.shape(forma_comida)
+        nuevo_segmento.color(color_comida)
         nuevo_segmento.penup()
         segmentos.append(nuevo_segmento)
 
@@ -104,36 +115,36 @@ while True:
 
         if score > high_score:
             high_score = score
-            
+
         texto.clear()
-        texto.write("Score: {}      High Score: {}".format(score, high_score), align = "center", font =("Courier", 24, "normal"))
+        texto.write("Score: {}      High Score: {}".format(score, high_score), align="center", font=("Courier", 24, "normal"))
 
     totalSeg = len(segmentos)
-    for index in range(totalSeg -1, 0, -1):
+    for index in range(totalSeg - 1, 0, -1):
         x = segmentos[index - 1].xcor()
         y = segmentos[index - 1].ycor()
-        segmentos[index].goto(x,y)
+        segmentos[index].goto(x, y)
 
     if totalSeg > 0:
         x = cabeza.xcor()
         y = cabeza.ycor()
-        segmentos[0].goto(x,y)
+        segmentos[0].goto(x, y)
 
     mov()
 
     for segmento in segmentos:
         if segmento.distance(cabeza) < 20:
             time.sleep(1)
-            cabeza.goto(0,0)
+            cabeza.goto(0, 0)
             cabeza.direction = "stop"
 
             for segmento in segmentos:
-                segmento.goto(2000,2000)
+                segmento.goto(2000, 2000)
 
             segmentos.clear()
 
             score = 0
             texto.clear()
-            texto.write("Score: {}      High Score: {}".format(score, high_score), align = "center", font =("Courier", 24, "normal"))
+            texto.write("Score: {}      High Score: {}".format(score, high_score), align="center", font=("Courier", 24, "normal"))
 
     time.sleep(posponer)
